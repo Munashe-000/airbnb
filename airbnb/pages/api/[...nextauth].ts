352edd10c482ba90f58eab42bfdab88e90624363
Munashe <theson831@gmail.com>
2024-10-11 08:12:05 +0200

    completed next auth class part 1

--- airbnb/pages/api/[...nextauth].ts
+++ airbnb/pages/api/[...nextauth].ts
@@ -3,8 +3,10 @@ import { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
 
 import prisma from "@/app/librabries/prisma_libs";
+import NextAuth from "next-auth";
 
 export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
@@ -38,7 +40,28 @@ export const authOptions: AuthOptions = {
                 if (!user || !user?.hashedPassword) {
                     throw new Error('Invalid credentials');
                 }
+
+                const isCorrectPassword = await bcrypt.compare(
+                    credentials.password,
+                    user.hashedPassword
+                );
+
+                if (!isCorrectPassword) {
+                    throw new Error('Invalid credentials');
+                }
+
+                return user;
             }
         })
-    ]
-}
+    ],
+    pages: {
+        signIn: '/',
+    },
+    debug: process.env.NODE_ENV === 'development',
+    session: {
+        strategy: "jwt"
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
